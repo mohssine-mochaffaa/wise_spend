1e1a742d40fb57b6f94526fd0b2dd358704ab999
Haytam04 <haytam.tanji123@gmail.com>
2025-01-18 23:32:38 +0100

    authentification-pages

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import InputField from '../../Components/InputField/InputField'
 import "./Login.css"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+    <div className="login-container">
+      <h2 className="form-title">Login</h2>
+      <p className="separator"></p>
+
+      <form action="#" className="login-form">
+        <InputField type="email" placeholder="Email address" icon="mail" />
+        <InputField type="password" placeholder="Password" icon="lock" />
+        
+        <button type="submit" className="login-button">Log In</button>
+      </form>
+
+      <p className="signup-prompt">
+        Don&apos;t have an account? <Link to="/signUp" className="signup-link">Sign up</Link>
+      </p>
+    </div>
+    </>
   )
 }
 
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import InputField from '../../Components/InputField/InputField'
 import "./SignUp.css"
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className="login-container">
+          <h2 className="form-title">Sign Up</h2>
+          <p className="separator"></p>
+    
+          <form action="#" className="login-form">
+            <InputField type="text" placeholder="Full Name" icon="id_card" />
+            <InputField type="email" placeholder="Email address" icon="mail" />
+            <InputField type="password" placeholder="Password" icon="lock" />
+    
+            <button type="submit" className="login-button">Sign Up</button>
+          </form>
+    
+          <p className="signup-prompt">
+            If you have an account? <Link to="/" className="signup-link">Login</Link>
+          </p>
+    </div>
   )
 }
 
